e6b5d6fdc82e5ef4860138cbb15aadebd355d608
sedatcakir26 <sedatcakir@gmail.com>
2022-08-07 22:31:22 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sedat Çakır on 7.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
